28c1ed0310c67d21714cbe552200e3a1e3ae096d
Jake Coffman <jake@jakecoffman.com>
2024-10-24 18:56:59 -0500

    fix deployment?

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: '404.html',
 			precompress: false,
 			strict: true,
 			paths: {
