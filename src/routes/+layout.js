70ad38e6ff32b5e1fc809e5d471487c0833d65ad
Jake Coffman <jake@jakecoffman.com>
2024-10-24 20:26:24 -0500

    fix deployment?

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,2 @@
-// This can be false if you're using a fallback (i.e. SPA mode)
-export const prerender = true;
+export const prerender = true; // set for a static build
+export const trailingSlash = 'always';
